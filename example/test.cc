a6dae17354264ecc6ef89722231236cf14f451c1
David Ding <david@Davids-MacBook-Pro.local>
2014-12-08 16:52:06 -0500

    timing functions

--- example/test.cc
+++ example/test.cc
@@ -5,11 +5,19 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <sys/resource.h>
 
 using cuckoofilter::CuckooFilter;
 
+double time_diff(struct rusage& start, struct rusage& end) {
+    return (end.ru_utime.tv_usec + end.ru_stime.tv_usec -
+        start.ru_utime.tv_usec - start.ru_stime.tv_usec) +
+    1000000 * (end.ru_utime.tv_sec + end.ru_stime.tv_sec -
+        start.ru_utime.tv_sec - start.ru_stime.tv_sec);
+}
+
 int main(int argc, char** argv) {
-    size_t total_items  = 1<<20;
+    size_t total_items  = 1<<24;
 
     // Create a cuckoo filter where each item is of type size_t and
     // use 12 bits for each item:
@@ -18,24 +26,37 @@ int main(int argc, char** argv) {
     // PackedTable, accepting keys of size_t type and making 13 bits
     // for each key:
     //   CuckooFilter<size_t, 13, PackedTable> filter(total_items);
-    CuckooFilter<size_t, 8> filter(total_items);
+    CuckooFilter<size_t, 12> filter(total_items);
 
     // Insert items to this cuckoo filter
     size_t num_inserted = 0;
+    struct rusage start;
+    struct rusage end;
+
+    getrusage(RUSAGE_SELF, &start);
     for (size_t i = 0; i < total_items; i++, num_inserted++) {
         if (filter.Add(i*i) != cuckoofilter::Ok) {
             break;
         }
     }
+    getrusage(RUSAGE_SELF, &end);
+
+    std::cout << "Construction throughput (MOPS): " << num_inserted/time_diff(start, end) << std::endl;
+
     // Check if previously inserted items are in the filter, expected
     // true for all items
     size_t counter = 0;
+
+    getrusage(RUSAGE_SELF, &start);
     for (size_t i = 0; i < num_inserted; i++) {
         if(filter.Contain(i*i) == cuckoofilter::Ok) {
             counter++;
-	}
+        }
     }
+    getrusage(RUSAGE_SELF, &end);
     
+    std::cout << "Positive query throughput (MOPS): " << num_inserted/time_diff(start, end) << std::endl;
+
     if( counter != num_inserted ) {
         std::cout << "Counter is " << counter << "while actual is " << num_inserted << std::endl;
     }
@@ -43,12 +64,17 @@ int main(int argc, char** argv) {
     // Check non-existing items, a few false positives expected
     size_t total_queries = 0;
     size_t false_queries = 0;
+
+    getrusage(RUSAGE_SELF, &start);
     for (size_t i = 0; i < total_items; i++) {
         if (filter.Contain(i*i+2) == cuckoofilter::Ok) {
             false_queries++;
         }
         total_queries++;
     }
+    getrusage(RUSAGE_SELF, &end);
+
+    std::cout << "Negative query throughput (MOPS): " << num_inserted/time_diff(start, end) << std::endl;
 
     // Output the measured false positive rate
     std::cout << "false positive rate is "
